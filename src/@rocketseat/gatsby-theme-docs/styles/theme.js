e571b72dbbcd2f28ba6bd57324a948bffe0569aa
PeHaa <info@pehaa.com>
2021-04-29 14:32:28 +0200

    initial commit

---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    primary: '#662e91',
+    accent: 'rgb(230,74,145)',
+    code: 'rgb(137,25,248)',
+    background: '#FFFFFF',
+    shape: `#f0e9f4`,
+    title: `#312935`,
+    text: `#5c4e67`,
+    components: {
+      blockquote: {
+        background: `#feeaf3`,
+      },
+    },
+  },
+};
